f0fdb683312fa2032d0742df63e555fba7c09b39
Drawcard <signup@drawcard.com.au>
2018-07-10 15:44:35 +0930

    Update with Zen code

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -1,105 +1,39 @@
 <?php
-
 /**
  * @file
- * Default theme implementation to display the basic html structure of a single
- * Drupal page.
+ * Returns the HTML for the basic html structure of a single Drupal page.
  *
- * Variables:
- * - $css: An array of CSS files for the current page.
- * - $language: (object) The language the site is being displayed in.
- *   $language->language contains its textual representation.
- *   $language->dir contains the language direction. It will either be 'ltr' or 'rtl'.
- * - $rdf_namespaces: All the RDF namespace prefixes used in the HTML document.
- * - $grddl_profile: A GRDDL profile allowing agents to extract the RDF data.
- * - $head_title: A modified version of the page title, for use in the TITLE
- *   tag.
- * - $head_title_array: (array) An associative array containing the string parts
- *   that were used to generate the $head_title variable, already prepared to be
- *   output as TITLE tag. The key/value pairs may contain one or more of the
- *   following, depending on conditions:
- *   - title: The title of the current page, if any.
- *   - name: The name of the site.
- *   - slogan: The slogan of the site, if any, and if there is no title.
- * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
- *   so on).
- * - $styles: Style tags necessary to import all CSS files for the page.
- * - $scripts: Script tags necessary to load the JavaScript files and settings
- *   for the page.
- * - $page_top: Initial markup from any modules that have altered the
- *   page. This variable should always be output first, before all other dynamic
- *   content.
- * - $page: The rendered page content.
- * - $page_bottom: Final closing markup from any modules that have altered the
- *   page. This variable should always be output last, after all other dynamic
- *   content.
- * - $classes String of classes that can be used to style contextually through
- *   CSS.
- *
- * @see template_preprocess()
- * @see template_preprocess_html()
- * @see template_process()
- *
- * @ingroup themeable
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728208
  */
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php
+?><!DOCTYPE html>
+<html <?php print $html_attributes . $rdf_namespaces; ?>>
+<head>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
 
-print $language->language;
-?>" version="XHTML+RDFa 1.0" dir="<?php
+  <?php if ($default_mobile_metatags): ?>
+    <meta name="MobileOptimized" content="width">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="viewport" content="width=device-width">
+  <?php endif; ?>
 
-print $language->dir;
-?>"<?php
-
-print $rdf_namespaces;
-?>>
-
-<head profile="<?php
-
-print $grddl_profile;
-?>">
-  <?php
-
-print $head;
-?>
-  <title><?php
-
-print $head_title;
-?></title>
-  <?php
-
-print $styles;
-?>
-  <?php
-
-print $scripts;
-?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <?php if ($add_html5_shim): ?>
+    <!--[if lt IE 9]>
+    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5shiv.min.js"></script>
+    <![endif]-->
+  <?php endif; ?>
 </head>
-<body class="<?php
-
-print $classes;
-?>" <?php
-
-print $attributes;
-?>>
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php
-
-print t('Skip to main content');
-?></a>
-  </div>
-  <?php
-
-print $page_top;
-?>
-  <?php
-
-print $page;
-?>
-  <?php
-
-print $page_bottom;
-?>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <?php if ($skip_link_text && $skip_link_anchor): ?>
+    <p class="skip-link__wrapper">
+      <a href="#<?php print $skip_link_anchor; ?>" class="skip-link visually-hidden visually-hidden--focusable" id="skip-link"><?php print $skip_link_text; ?></a>
+    </p>
+  <?php endif; ?>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
 </body>
 </html>
